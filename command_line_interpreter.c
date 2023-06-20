bffc62c0145052d7f8d7646e034fe0ec4f5c47e3
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-06-20 10:29:45 -0700

    A UNIX command line interpreter

---
+++ command_line_interpreter.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - function
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	while (1)
+	{
+	char *word;
+	size_t length;
+	ssize_t r;
+
+	printf("Waiting for your command: \n");
+	r = getline(&word, &length, stdin);
+
+	if (r == -1)
+	{
+		perror("Error");
+		exit(1);
+	}
+	else if (r != -1)
+	{
+		printf("Command read: %s", word);
+	}
+
+	free(word);
+	}
+
+	return (0);
+}
