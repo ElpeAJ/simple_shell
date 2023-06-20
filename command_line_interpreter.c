5be0b96a0da4d29f613c34998c6597026f1b7fe3
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-06-20 14:06:20 -0700

    A UNIX command line interpreter

--- command_line_interpreter.c
+++ command_line_interpreter.c
@@ -12,24 +12,28 @@
 
 int main(void)
 {
+	char *args[] = {" ", NULL};
+
 	while (1)
 	{
-	char *word;
-	size_t length;
-	ssize_t r;
+		char *word = NULL;
+		size_t length = 0;
+		ssize_t r = 0;
 
-	printf("Waiting for your command: \n");
-	r = getline(&word, &length, stdin);
+		printf("Waiting for your command: \n");
+		r = getline(&word, &length, stdin);
 
-	if (r == -1)
-	{
-		perror("Error");
-		exit(1);
-	}
-	else if (r != -1)
-	{
-		printf("Command read: %s", word);
-	}
+		if (r == -1)
+		{
+			perror("Error");
+			exit(1);
+		}
+		else if (r != -1)
+		{
+			execve(word, args, __environ);
+			perror("execve");
+			exit(1);
+		}
 	}
 
 	return (0);
