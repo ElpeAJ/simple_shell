b12c52b68773df6a59e3d9a7d0d876132aca496d
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-01 03:11:58 -0700

    A UNIX line interpreter plus env builtin

--- _should_env.c
+++ _should_env.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
-  * _should_env - function that prints the current environment
-  * @command: user prompt
-  *
-  * Return: nothing
-  */
+ * _should_env - function that prints the current environment
+ * @command: user prompt
+ *
+ * Return: nothing
+ */
 
 void _should_env(char *command)
 {
 	int i = 0;
 
-	if (command[0] == 'e' && command[1] == 'n' &&
-			command[2] == 'v')
+	if (strcmp(command, "env") == 0)
 	{
 		while (environ[i] != NULL)
 		{
-			printf("current environment: %s\n", environ[i]);
+			printf("%s\n", environ[i]);
 			i++;
 		}
 	}
